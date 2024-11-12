b86d20aed9a6fa32fe535835d30ca5057ba156c9
TeddyPrivat <47928474+TeddyPrivat@users.noreply.github.com>
2024-11-12 10:27:06 +0100

    message d'erreur dans le cas où la recherche ne contient aucun élément mis à jour

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ import { createRecette, truncateTextByHeight } from "./recette.js";
 const url = 'https://gist.githubusercontent.com/baiello/0a974b9c1ec73d7d0ed7c8abc361fc8e/raw/e598efa6ef42d34cc8d7e35da5afab795941e53e/recipes.json'
 
 let recettes = [];
+let ingredients = [];
 
 function getData() {
   return fetch(url)
@@ -24,11 +25,11 @@ getData().then(
 function displayRecettes(recettes) {
   const recettesContainer = document.getElementById('recettes');
   const nbResults = document.getElementById('nbResults');
-  recettesContainer.innerHTML = "";  // Vider l'affichage précédent
+  recettesContainer.innerHTML = "";  
   if(recettes.length === 0){
     const noResultMessage = document.createElement('h3');
     noResultMessage.style.fontFamily = "'Protest Strike', sans-serif";
-    noResultMessage.textContent = "Aucune recette à afficher";
+    noResultMessage.textContent = "Aucune recette ne contient " + searchInput.value + " essayez plutôt 'tarte aux pommes'";
     recettesContainer.appendChild(noResultMessage);
     nbResults.textContent = ""
   }else{
